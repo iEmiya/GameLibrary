697efa7071cfd9a4af3139def8bd95693e1d136e
Emiya <flin@ya.ru>
2016-07-13 11:47:07 +0300

    Объединение HotswapClassAttribute с файлом HotswapCSharpScript

--- src/Core/HotswapCSharpScript.cs
+++ src/Core/HotswapCSharpScript.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -359,5 +360,22 @@ namespace GameLibrary
 		}
 
 		#endregion
+	}
+
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+	public sealed class HotswapClassAttribute : Attribute
+	{
+		public HotswapClassAttribute(Type interfaceType, string classFileSource, bool overwriteWhenBoot = false)
+		{
+			Contract.Assert(interfaceType != null);
+			Contract.Assert(!string.IsNullOrEmpty(classFileSource));
+			InterfaceType = interfaceType;
+			ClassFileSource = Path.GetFullPath(classFileSource);
+			OverwriteWhenBoot = overwriteWhenBoot;
+		}
+
+		public Type InterfaceType { get; }
+		public string ClassFileSource { get; }
+		public bool OverwriteWhenBoot { get; }
 	}
 }
